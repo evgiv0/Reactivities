b086d45dfb42de6f3283b9bf4cc6a911d4d53753
evgiv0 <zhe956@gmail.com>
2020-11-22 22:04:00 +0300

    End of section 5

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,11 @@
+import { StringLiteral } from "typescript";
+
+export interface IActivity{
+    id: string;
+    title: string;
+    category: string;
+    description: string;
+    date: string;
+    city: string;
+    venue: string;
+}
