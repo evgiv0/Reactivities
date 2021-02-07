482b2134f5cb9df5f18cccb061c2d106a2b2bbe5
evgiv0 <zhe956@gmail.com>
2021-02-07 23:06:32 +0300

    End of section 12

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,29 +1,31 @@
-import { observer } from 'mobx-react-lite'
-import React, { useContext, useEffect } from 'react'
-import { Grid } from 'semantic-ui-react'
-import LoadingComponent from '../../../app/layout/LoadingComponent'
-import ActivityStore from '../../../app/stores/ActivityStore'
-import ActivityList from './ActivityList'
+import React, { useContext, useEffect } from 'react';
+import { Grid } from 'semantic-ui-react';
+import ActivityList from './ActivityList';
+import { observer } from 'mobx-react-lite';
+import ActivityStore from '../../../app/stores/activityStore';
+import LoadingComponent from '../../../app/layout/LoadingComponent';
 
 const ActivityDashboard: React.FC = () => {
-    const activityStore = useContext(ActivityStore)
 
-    useEffect(() => {
-        activityStore.loadActivities()
-    }, [activityStore]);
+  const activityStore = useContext(ActivityStore);
 
-    if (activityStore.loadingInitial) return <LoadingComponent content="Loading activities..." />
+  useEffect(() => {
+    activityStore.loadActivities();
+  }, [activityStore]);
 
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <ActivityList />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <h2>Activity filters</h2>
-            </Grid.Column>
-        </Grid>
-    )
-}
+  if (activityStore.loadingInitial)
+    return <LoadingComponent content='Loading activities' />;
 
-export default observer(ActivityDashboard)
+  return (
+    <Grid>
+      <Grid.Column width={10}>
+        <ActivityList />
+      </Grid.Column>
+      <Grid.Column width={6}>
+        <h2>Activity filters</h2>
+      </Grid.Column>
+    </Grid>
+  );
+};
+
+export default observer(ActivityDashboard);
